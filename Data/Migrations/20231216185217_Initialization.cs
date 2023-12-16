d757d0bea712024bba8feb5456af9697facacc59
Oleksandr Kutsenko <okutsenko@solvexus.com>
2023-12-17 00:44:48 +0200

    Created ASP.NET Core MVC application with simple Google server authentication

---
+++ Data/Migrations/20231216185217_Initialization.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GoogleAuthMVCApp.Data.Migrations
+{
+    public partial class Initialization : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
